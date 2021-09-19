f9a68d3351be6d9cc06e9d23101e1aedbf4c627e
abdvool <abdvool@gmail.com>
2021-09-19 21:36:03 +0300

    finish lb6

--- city-explorer/src/App.js
+++ city-explorer/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+class App extends React.Component {
+  constructor(props) {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    super(props);
+    this.state = {
+      locationResult: {},
+      seacrhQuery: '',
+
+      showDetails: false
+
+    }
+  }
+
+  getlocfun = async (e) => {
+
+    e.preventDefault();
+    // let cityName = e.target.city.value;
+    let reqUrl = `https://us1.locationiq.com/v1/search.php?key=pk.ba36a331f9eab6fca379d35672ec3837
+
+&q=${this.state.seacrhQuery}&format=json`;
+
+
+    await this.setState({
+
+      seacrhQuery: e.target.city.value
+    })
+    let locResult = await axios.get(reqUrl);
+    console.log('a', locResult)
+    console.log('b', locResult.data)
+    console.log('c', locResult.data[0])
+
+
+    this.setState({
+
+      locationResult: locResult.data[0],
+
+      showDetails: true
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h3>city Explorer app</h3>
+        {/* <button onClick={this.getlocfun}>Get location</button> */}
+        <form onSubmit={this.getlocfun}>
+          <input type="text" name='city' />
+          <input type="submit" value='city information' />
+
+
+        </form>
+
+        {
+        this.state.showDetails &&
+        <>
+ 
+        <p>City name: {this.state.seacrhQuery}</p>
+        <p>latitude {this.state.locationResult.lat} </p>
+        <p>longitude {this.state.locationResult.lon} </p>
+
+
+<img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`}  alt="" />
+</>
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
